93bd1be40b9a5173c4499d49ebbb2867f7234883
Ricardo Sebastian Youssef <ricardoy1@gmail.com>
2017-01-05 23:02:47 +1100

    `Improved error messages.

--- HypoMail.Ui/MailFramework/MailResponse.cs
+++ HypoMail.Ui/MailFramework/MailResponse.cs
@@ -41,31 +41,41 @@
             switch (this.HttpResponse.StatusCode)
             {
                 case HttpStatusCode.BadRequest:
-                    return "The e-mail that you are trying to send contains errors that prevents it from being sent.";
+                    return "The e-mail that you are trying to send is not valid and the e-mail server has rejected it. "
+                           + "Please, make sure you have completed all mandatory fields and make sure those fields are valid: i.e: the e-mail address is a valid e-mail address.";
 
                 case HttpStatusCode.Unauthorized:
-                    return "The user is not authorized to send an e-mail. This is because the credentials are not valid or expired.";
+                    return "The user is not authorized to send an e-mail. This is because the credentials are not valid or have expired. "
+                           + "Please, contact technical support to resolve this problem.";
 
                 case HttpStatusCode.Forbidden:
-                    return "The e-mail server has rejected the connection.";
+                    return "The e-mail server has rejected the connection. This could be happening due to a problem on the e-mail server "
+                           + "or because the e-mail server connection has not been properly set. Please, contact technical support to resolve this problem.";
 
                 case HttpStatusCode.NotFound:
-                    return "The e-mail service could not be found.";
+                    return "The e-mail service could not be found. This could be happening due to a problem on the e-mail server "
+                           + "or because the e-mail server connection has not been properly set. Please, contact technical support to resolve this problem.";
 
                 case HttpStatusCode.MethodNotAllowed:
-                    return "The e-mail service has rejected the operation due to lack of permissions.";
+                    return "The e-mail service has rejected the operation due to lack of permissions. It is possible that the e-mail server connection has not been properly set. "
+                           + "Please, contact technical support to resolve this problem.";
 
                 case HttpStatusCode.RequestEntityTooLarge:
                     return "The e-mail that you are trying to send is too big and it was rejected by the e-mail server.";
 
                 case HttpStatusCode.InternalServerError:
-                    return "An error occurred in the e-mail server while your e-mail was being processed.";
+                    return "An error occurred in the e-mail server while your e-mail was being processed. If the problem persists, please contact technical support to resolve this problem.";
 
                 case HttpStatusCode.ServiceUnavailable:
-                    return "The e-mail server is not currently available. Please, try again in a few moments.";
+                    return "The e-mail server is not currently available. Please, try again in a few moments. If the problem persists, please contact technical support to resolve this problem.";
+
+                case HttpStatusCode.RequestTimeout:
+                    return
+                        "The e-mail server has timed-out while trying to send your e-mail. Please, try again in a few moments. If the problem persists, please contact technical support to resolve this problem.";
 
                 default:
-                    return "Uknown error while trying to send the e-mail.";
+                    return "Uknown error while trying to send the e-mail: There has been an error with code: Http-" + this.HttpResponse.StatusCode + " "
+                           + "Please, contact technical support providing with those details.";
             }
         }
     }
